86219e5d211e7189792572266766b972ffd26b43
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-05-18 19:57:04 +0300

    Add price and title with some styling

--- eshop/src/products/Products.jsx
+++ eshop/src/products/Products.jsx
@@ -19,14 +19,16 @@ function Products() {
     <ul className="flex flex-wrap">
       {products.map((x) => (
         <li key={x.id} className="p-5">
-          <div className="bg-white border shadow-sm rounded w-64 h-80">
+          <div className="bg-white border shadow-sm rounded w-64 h-80 p-2">
             <div className="h-3/4">
               <img src={x.image} alt={x.name} className="max-h-full m-auto" />
             </div>
 
-            <div></div>
+            <div className="text-center text-lg font-semibold mt-1">{x.price}$</div>
 
-            <div></div>
+            <div className=" text-center text-lg truncate mt-1 font-bold" title={x.title}>
+              {x.title}
+            </div>
           </div>
         </li>
       ))}
