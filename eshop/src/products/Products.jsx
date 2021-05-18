a625377848eb1f02ad2f450c465641adabfa9e0f
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-05-18 19:34:22 +0300

    show product cards

--- eshop/src/products/Products.jsx
+++ eshop/src/products/Products.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 function Products() {
   const [products, setProducts] = useState([]);
 
+  console.log(products);
+
   useEffect(() => {
     const getProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
@@ -14,9 +16,11 @@ function Products() {
   }, []);
 
   return (
-    <ul>
+    <ul className="flex flex-wrap">
       {products.map((x) => (
-        <li key={x.id}>{x.title}</li>
+        <li key={x.id} className="p-5">
+          <div className="bg-white border shadow-sm rounded w-64 h-80"></div>
+        </li>
       ))}
     </ul>
   );
