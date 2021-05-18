04683f71952c72e99a76919eb4fc4145a6731103
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-05-18 18:39:19 +0300

    Counter handler implementation

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -3,11 +3,29 @@ import { useState } from 'react';
 function Counter() {
   const [count, setCount] = useState(1);
 
+  function handleIncrement() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function handleDecrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function handleInputChange(e) {
+    const val = e.target.value;
+
+    if (!val) {
+      setCount(1);
+    } else {
+      setCount(parseInt(val));
+    }
+  }
+
   return (
     <div>
-      <button>-</button>
-      <input value={count} />
-      <button>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <input type="number" value={count} onChange={handleInputChange} />
+      <button onClick={handleIncrement}>+</button>
     </div>
   );
 }
