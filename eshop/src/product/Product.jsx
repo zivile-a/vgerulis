96eaf35a4a07137b904449857bde402c12ea685b
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-06-01 18:26:17 +0300

    Call api to get single product info

--- eshop/src/product/Product.jsx
+++ eshop/src/product/Product.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Product() {
   const { id } = useParams();
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const product = await response.json();
+
+      setProduct(product);
+    };
+
+    getProduct();
+  }, [id]);
 
   return <div>Product Page {id}</div>;
 }
