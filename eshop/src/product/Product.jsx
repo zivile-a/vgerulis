f23f71db234d3f20656f4f79cf91d5b57604b9ef
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-06-01 19:49:59 +0300

    Add product initial styling

--- eshop/src/product/Product.jsx
+++ eshop/src/product/Product.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import { getProduct } from '../common/requests';
+import Card from '../common/components/Card';
+import Counter from '../common/components/Counter';
+import Button from '../common/components/Button';
 
 function Product() {
   const { id } = useParams();
@@ -17,7 +20,28 @@ function Product() {
     fetchProduct();
   }, [id]);
 
-  return <div>Product Page {id}</div>;
+  return (
+    <div className="flex m-auto px-2" style={{ maxWidth: 1000 }}>
+      <div className="pr-6 max-w-1/2">
+        <Card>
+          <img src={product?.image} alt={product?.title} />
+        </Card>
+      </div>
+
+      <div className="pl-6 max-w-1/2">
+        <h1 className="text-2xl mb-4 font-semibold">{product?.title}</h1>
+
+        <div className="text-3xl mb-4 font-bold">{product?.price}$</div>
+
+        <p className="mb-4">{product?.description}</p>
+
+        <div className="mb-2 font-semibold">Quantity</div>
+        <Counter />
+
+        <Button>ADD TO CART</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
